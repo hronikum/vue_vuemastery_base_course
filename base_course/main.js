1dca73c6dc344471f6c30a8492eed55489f1658f
Zhostya Kilin <kostya.zhilin@gmail.com>
2020-05-29 12:10:10 +0300

    1 and 2 lessons, v-bind, {{}}  and : learned

---
+++ base_course/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Boots',
+    description: 'This is the best description ever',
+    image: 'vmSocks-green-onWhite.jpg',
+    alt: 'The best alt ever'
+  }
+})
