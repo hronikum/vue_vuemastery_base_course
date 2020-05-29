222ebe7132c12913423410e993f469b50c4e55b8
Zhostya Kilin <kostya.zhilin@gmail.com>
2020-05-29 13:07:07 +0300

    3,4,5 lessons. Learned: v-for, events, methods, v-show.

--- base_course/main.js
+++ base_course/main.js
@@ -1,9 +1,35 @@
-var app = new Vue({
+let app = new Vue({
   el: '#app',
   data: {
     product: 'Boots',
     description: 'This is the best description ever',
     image: 'vmSocks-green-onWhite.jpg',
-    alt: 'The best alt ever'
-  }
+    inStock: true,
+    onSale: false,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: 'vmSocks-green-onWhite.jpg',
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+        variantImage: 'vmSocks-blue-onWhite.jpg',
+      },
+    ],
+    cart: 0,
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    removeFromCart() {
+      this.cart -= 1
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+    },
+  },
 })
